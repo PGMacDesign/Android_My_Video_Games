e8cdc5406dcab3f56930175ea6d3ae3d9060af36
pgmacdesign <pgmacdesign@gmail.com>
2015-02-05 19:40:24 -0800

    Adding some framework first. Putting in packages and classes that will be used

---
+++ app/src/main/java/com/pgmacdesign/myvideogames/ListFragment.java
@@ -0,0 +1,9 @@
+package com.pgmacdesign.myvideogames;
+
+import android.app.Fragment;
+
+/**
+This class will server as the fragment that works with the listview to have data
+ */
+public class ListFragment extends Fragment{
+}
---
+++ app/src/main/java/com/pgmacdesign/myvideogames/gson/ExampleCalls.java
@@ -0,0 +1,18 @@
+package com.pgmacdesign.myvideogames.gson;
+
+/**
+Just some example calls for my reference. Will have to come back and erase this before the end
+ */
+public class ExampleCalls {
+
+	/*
+
+	//This searches for the game and replies a list of games that match the search string (IE FF7)
+	http://www.giantbomb.com/api/search/?api_key=9be0ead91d814eeb64cc5fb0da481ce726a22400&query=final_fantasy_VII&field_list=name,id,api_detail_url&format=json
+
+	//This brings up the info for the game in question, the game ID is retrieved from the search for game call (IE FF7)0
+	http://www.giantbomb.com/api/games/?api_key=9be0ead91d814eeb64cc5fb0da481ce726a22400&filter=id:13053&format=json
+
+
+	 */
+}
