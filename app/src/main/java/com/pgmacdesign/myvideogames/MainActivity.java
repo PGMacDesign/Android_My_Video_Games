387b345915a3b3681ede51b49aafe332405eeb54
pgmacdesign <pgmacdesign@gmail.com>
2015-02-05 23:54:21 -0800

    Added action bars for respective activities. Added plus button for add new game option. Left off plus button on rate games activity. Added documentation in mainActivity

--- app/src/main/java/com/pgmacdesign/myvideogames/MainActivity.java
+++ app/src/main/java/com/pgmacdesign/myvideogames/MainActivity.java
@@ -12,6 +12,7 @@ to the RateGamesActivity. There will be NO action bar on purpose.
  */
 public class MainActivity extends Activity implements View.OnClickListener {
 
+	//2 Image Buttons. When clicked, they will open up the respective activity
 	ImageButton rank_my_games_button, list_my_games_button;
 
 	@Override
@@ -20,8 +21,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		setContentView(R.layout.activity_main);
 		//Initialize any and all variables / views / buttons needed
 		Initialize();
-
-
 	}
 
 	private void Initialize() {
@@ -30,11 +29,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		rank_my_games_button = (ImageButton) findViewById(R.id.rank_my_games_button);
 		list_my_games_button = (ImageButton) findViewById(R.id.list_my_games_button);
 
+		//Set them to onClickListeners so they can be clicked for an action
 		list_my_games_button.setOnClickListener(this);
 		rank_my_games_button.setOnClickListener(this);
 
 		//Create the database if it has not been created already
-
+		//createADatabase();
 	}
 
 	@Override
@@ -42,15 +42,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		//Switch case to open the respective activity that the user chooses.
 		switch (v.getId()) {
 
+			//If they click the list_my_games option, open respective activity
 			case R.id.list_my_games_button:
 				try {
-					Intent intent00 = new Intent(v.getContext(), ListGamesActivity.class);
-					startActivity(intent00);
+					Intent intent0 = new Intent(v.getContext(), ListGamesActivity.class);
+					startActivity(intent0);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				break;
-			
+
+			//If they click the rank_my_games option, open respective activity
 			case R.id.rank_my_games_button:
 				try {
 					Intent intent1 = new Intent(v.getContext(), RateGamesActivity.class);
--- app/src/main/java/com/pgmacdesign/myvideogames/RateGamesActivity.java
+++ app/src/main/java/com/pgmacdesign/myvideogames/RateGamesActivity.java
@@ -36,18 +36,8 @@ public class RateGamesActivity extends ActionBarActivity {
 		return true;
 	}
 
-	@Override
+	//No click-able items in this action bar
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
 		return super.onOptionsItemSelected(item);
 	}
 
